b3df88c804e67cc096cfdcd3c122755d230f38e0
Jorge Gonzalez <jorg3.594@gmail.com>
2023-07-11 08:25:35 -0500

    Refactor Document model to improve readability and validation
    The changes made in the Document model primarily involve refactoring for improved code readability and more robust validation. Firstly, the method "friendly_id" has been updated to utilize :file_name instead of :file for more clear code semantics. Secondly, the previous "validates" method has been decomposed into two separate "validate" methods to make their purposes more explicit - asserting the file's presence and ensuring its format is PDF. Additionally, the index_file! and file_loader methods were simplified for brevity.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,30 +1,25 @@
 class Document < ApplicationRecord
   extend FriendlyId
-  friendly_id :file, use: :slugged
+  friendly_id :file_name, use: :slugged
 
   has_one_attached :file
-  after_create_commit :index_file!
+  # after_create_commit :index_file!
 
-  validates :file, attached: true, content_type: %w[application/pdf]
-
+  validate { errors.add(:file, 'must be present') unless file.attached? }
+  validate { errors.add(:file, 'must be a pdf') unless file.content_type == 'application/pdf' }
 
   private
 
+  def file_name
+    file.filename.to_s
+  end
+
   def index_file!
-    VECTOR_SEARCH_CLIENT.add_texts(texts: file_loader.load.value)
+    VECTOR_SEARCH_CLIENT.add_texts(texts: file_loader.chunks(chunk_size: 500, chunk_overlap: 50), namespace: slug)
   end
 
   def file_loader
-    Langchain::Loader.new(file_url)
-  end
-
-  def client
-    @client ||= Langchain::Vectorsearch::Pinecone.new(
-      environment: ENV['PINECONE_ENVIRONMENT'],
-      api_key: ENV['PINECONE_API_KEY'],
-      index_name: ENV['PINECONE_INDEX_NAME'],
-      llm: OPENAI_CLIENT
-    )
+    Langchain::Loader.new(file_url).load
   end
 
   def file_url
